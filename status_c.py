500e20e48dc928be62bec0395f7a0b1f9cb34737
dekarh <dekarh3@gmail.com>
2017-06-21 21:53:18 +0400

    Заполняю массивы

--- status_c.py
+++ status_c.py
@@ -7,9 +7,22 @@ import csv
 
 workbook = openpyxl.load_workbook(filename=sys.argv[1], read_only=True)
 sheet = workbook.active
+g = 0
 for i, row in enumerate(sheet.rows):
-        for cell in row:
-            print(cell.value)
+        for j, cell in enumerate(row):
+            g=0
 
-# open('input.txt', 'r',  encoding='cp1251')
-
+stat_our2csv = [{'Имя':'Он они он','Возраст':25,'Вес':200},
+         {'Имя':'Я я я','Возраст':31,'Вес':180}]
+stat_fond2csv = [{'Имя':'Он они он','Возраст':25,'Вес':200},
+         {'Имя':'Я я я','Возраст':31,'Вес':180}]
+with open('stat_our.csv', 'w', encoding='cp1251') as output_file:
+    dict_writer = csv.DictWriter(output_file, stat_our2csv[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
+    dict_writer.writeheader()
+    dict_writer.writerows(stat_our2csv)
+output_file.close()
+with open('stat_fond.csv', 'w', encoding='cp1251') as output_file:
+    dict_writer = csv.DictWriter(output_file, stat_fond2csv[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
+    dict_writer.writeheader()
+    dict_writer.writerows(stat_fond2csv)
+output_file.close()
